5af06aab52c95e5d0639249ade819b152d29572d
bweirdooby-arch <bweirdooby@gmail.com>
2026-09-25 13:45:31 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "workout-tracker-cache-v1";
+const CACHE_NAME = "nutripulse-cache-v6";
 const ASSETS = [
   "./",
   "./index.html",
@@ -6,8 +6,19 @@ const ASSETS = [
 ];
 
 self.addEventListener("install", e => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
   );
 });
 
